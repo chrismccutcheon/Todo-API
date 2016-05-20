c492c77a4961422480417cb2f8019ce5d13484ef
Chris McCutcheon <cmccutcheon2015@gmail.com>
2016-05-19 18:43:32 -0700

    Added Post for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  desc: 'Get haircut',
-  completed: false
-},{
-  id: 2,
-  desc: 'Get ready for Chico',
-  completed: false
-},{
-  id: 3,
-  desc: 'Drink Burr',
-  completed: false
-}];
+var todoNextId = 1;
+
+var todos = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.send('Todo API root')
@@ -37,6 +30,15 @@ app.get('/todos/:id', function(req, res){
   } else {
     res.status(404).send();
   }
+});
+//post
+app.post('/todos', function(req, res){
+  var body = req.body;
+  console.log('Desc: ' + body.desc);
+  body.id = todoNextId;
+  ++todoNextId;
+  todos.push(body);
+  res.json(body);
 });
 
 app.listen(PORT, function(){
