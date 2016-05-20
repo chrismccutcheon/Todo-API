ba8fb841eaf24587661042f3ee3e442c41c03429
Chris McCutcheon <cmccutcheon2015@gmail.com>
2016-05-19 19:13:29 -0700

    Refactored Post with underscore.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 var todoNextId = 1;
 
@@ -18,13 +19,10 @@ app.get('/todos', function(req, res){
 });
 //Get /todos/:id
 app.get('/todos/:id', function(req, res){
-  var id =  parseInt(req.params.id);
+  var todoId =  parseInt(req.params.id);
   var matched;
-  todos.forEach(function(todo){
-    if(todo.id === id){
-      matched = todo;
-    }
-  });
+  matched = _.findWhere(todos, {id: todoId}); // this function searches the array for an object that matches the description object
+
   if(matched){
     res.send(matched);
   } else {
@@ -33,7 +31,12 @@ app.get('/todos/:id', function(req, res){
 });
 //post
 app.post('/todos', function(req, res){
-  var body = req.body;
+  var body = _.pick(req.body, 'desc', 'completed'); // this limits the fields to desc and completed
+
+  if(!_.isBoolean(body.completed) || !_.isString(body.desc) || body.desc.trim().length === 0){ // runs if body.competed is not a boolean or body.desc not a string
+    return res.status(400).send();
+  }
+  body.desc = body.desc.trim(); // take whitespace off of desc
   console.log('Desc: ' + body.desc);
   body.id = todoNextId;
   ++todoNextId;
